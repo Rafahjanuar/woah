21cfc5a88f0eaf3f8c3bda368fb98becfda98f0f
Rafahjanuar <103558850+Rafahjanuar@users.noreply.github.com>
2022-04-13 14:46:12 +0700

    Create session.js

---
+++ session.js
@@ -0,0 +1,5 @@
+{
+
+	"clientID": "PE/oQM+94vYH3R3bwUEUww==",	"serverToken": "1@GpGU0OYUanS9NrwMz6eQN79+XnHw10mK+vVfSOQmoYC7qCc8WreU0I2R8yif56oVHBaTbm6gz/y0tg==",
+
+	"clientToken": "WikUoTAodD/Q2IVksc5AsNmXop/9jWIp0BL3ER
